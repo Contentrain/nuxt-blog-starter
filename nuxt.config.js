565c3fcc4116b1ba4367a9519fce6765a44b3e4f
ABB65 <bayhanbayramoglu@gmail.com>
2022-04-09 01:27:43 +0300

    fix meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,14 +4,14 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'nuxt-blog-starter',
+    title: 'Contentrain NuxtJS Blog Starterkit',
     htmlAttrs: {
       lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Contentrain NuxtJS Blog Starterkit project.' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
